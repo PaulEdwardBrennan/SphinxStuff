cfe7072ee88ad3a8e3a1caf6c402ccc5d2c14dd7
paulebrennan@gmail.com <paulebrennan@gmail.com>
2022-08-18 14:36:17 +0100

    Add sphinx_design

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
+    'sphinx_design'
     'sphinx.ext.githubpages']
 
 # source_suffix = ['.rst', '.md']
